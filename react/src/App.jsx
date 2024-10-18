2b5e355ee779d07672b45b60383cf41bd54d9abd
Ron <you@example.com>
2024-10-18 16:07:09 +0800

    Bug

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "../components/Home"
-import Layout from "../components/Layout"
+import Home from "./components/Home";
+import Layout from "./components/Layout";
 
 export default function App() {  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout/>} />
+        <Route path="/" element={<Layout />}/>
         <Route index element={<Home/>} />
       </Routes>
     </BrowserRouter>
--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -8,12 +8,7 @@ const Layout = () => {
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
-                    <Link to="/post">Post User</Link>
-                </li>
-                <li>
-                    <Link to="/get">Get All User</Link>
-                </li>
+
             </ul>
         </nav>
         <Outlet/>
